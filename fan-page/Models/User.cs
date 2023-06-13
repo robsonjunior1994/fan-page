1f5c416a21339a992b241bad2cff1657a36db3cb
Robson Junior <robsonjunior1994@gmail.com>
2023-06-12 22:49:43 -0300

    Definindo a primeira classe user.

---
+++ fan-page/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fan_page.Models
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [RegularExpression("@\"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}$\"\r\n")]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmePassword { get; set; }
+        [Required]
+        [RegularExpression("@\"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$\"\r\n")]
+        public string Email { get; set; }
+        [Required]
+        [Compare("Email")]
+        public string ConfirmeEmail { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string ProfileImage { get; set; }
+        public enum Type 
+        {
+            Producer,
+            Consumer
+        }
+    }
+}
