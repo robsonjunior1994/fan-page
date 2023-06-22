fedfd648f3b2edaa70c790cf59e87b4c1f6314c8
Robson Junior <robsonjunior1994@gmail.com>
2023-06-22 20:38:36 -0300

    Incluindo DataAnnotations

--- fan-page/Models/Comentario.cs
+++ fan-page/Models/Comentario.cs
@@ -1,9 +1,12 @@
-﻿namespace fan_page.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fan_page.Models
 {
     public class Comentario : ClasseBase
     {
         public int IdUsuario { get; set; }
         public int IdPublicacao { get; set; }
+        [Required]
         public string Conteudo { get; set; }
         public DateTime DataDeCriacao { get; set; }
     }
--- fan-page/Models/Postagem.cs
+++ fan-page/Models/Postagem.cs
@@ -1,8 +1,11 @@
-﻿namespace fan_page.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fan_page.Models
 {
     public class Postagem : ClasseBase
     {
         public int IdUsario { get; set; }
+        [Required]
         public string Conteudo { get; set; }
         public DateTime DataDeCriacao { get; set; }
         public List<int> NumeroDeCurtidas { get; set; }
